504c20f61a341d5c197c75c2815e8d0b76721823
giullianacestari <dev.giullianacestari@gmail.com>
2024-03-19 18:12:27 -0300

    aula04

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
 // Cria a lista "tabela"
 let tabela = [
-    {
-      disciplina: "Matemática",
-      descricao: "página 29, exercícios 2 e 3",
-      data: "2024-08-02",
-      feito: false,
-      id: "ad980a9a-d480-e807-b06d-0f1df82337ad",
-    },
+  {
+    disciplina: "Matemática",
+    descricao: "página 29, exercícios 2 e 3",
+    data: "2024-08-02",
+    feito: false,
+    id: "ad980a9a-d480-e807-b06d-0f1df82337ad",
+  },
 ];
 
 // adiciona uma nova tarefa na lista "tabela"
@@ -25,5 +25,30 @@ function novaTarefa() {
   };
 
   tabela.push(novoRegistro);
-  console.log(tabela);
+  desenhaTabela();
 }
+
+// Desenhando a tabela com JS
+function desenhaTabela(listaTarefas = tabela) {
+  let tableBody = document.querySelector("tbody");
+
+  // limpando a tabela
+  tableBody.innerHTML = "";
+
+  // adicionando linhas e colunas
+  listaTarefas.forEach(function (item) {
+    let row = tableBody.insertRow();
+    let disciplinaCell = row.insertCell(0);
+    let descricaoCell = row.insertCell(1);
+    let dataCell = row.insertCell(2);
+    let feitoCell = row.insertCell(3);
+
+    disciplinaCell.textContent = item.disciplina;
+    descricaoCell.textContent = item.descricao;
+    dataCell.textContent = item.data;
+    feitoCell.textContent = item.feito ? "Sim" : "Não";
+  });
+}
+
+//desenhando assim que carrega a página
+desenhaTabela();
